87b8398289f30f924022f94c270ed9825494405e
Asif3029 <79526362+Asif3029@users.noreply.github.com>
2023-11-14 02:09:33 +0600

    Delete database directory

--- database/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("DB_HOST","localhost");
-define("DB_UNAME","DATABASE_USER_NAME");
-define("DB_PASS","DATABASE_PASSWORD");
-define("DB_DNAME","470sample");
-$conn=mysqli_connect(DB_HOST,DB_UNAME,DB_PASS,DB_DNAME);
-?>
